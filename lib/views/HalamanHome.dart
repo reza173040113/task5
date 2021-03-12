c6905097781a8c27298056ca1239e8951c74cf6f
reza173040113 <reza_173040113@mail.unpas.ac.id>
2021-03-12 15:02:46 +0700

    get data insyaallah bener

--- lib/views/HalamanHome.dart
+++ lib/views/HalamanHome.dart
@@ -1,108 +1,56 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:task5/views/HalamanPariwisata.dart';
 
+import 'HalamanPariwisata.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Halaman Utama",
+    home: new HalamanHome(),
+  ));
+}
 
 class HalamanHome extends StatefulWidget {
-  // final VoidCallback signOut;
-  // HalamanHome(this.signOut);
   @override
   _HalamanHomeState createState() => _HalamanHomeState();
 }
 
 class _HalamanHomeState extends State<HalamanHome> {
-  //tambahan method signout
-  // signOut() async {
-  //   setState(() {
-  //     widget.signOut();
-  //   });
-  // }
-  Future<List> getData() async {
-    final response =
-        await http.get("http://192.168.18.4/flutter-server/getBerita.php");
-    return json.decode(response.body);
-  }
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    HalamanPariwisata(),
+    HalamanPariwisata(),
+    HalamanPariwisata(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Halaman Home'),
-          backgroundColor: Colors.brown,         
-        ),
-        body:  Center(
-          child: Container(
-              color: Color.fromARGB(100, 239,227,212),
-              margin: EdgeInsets.only(bottom: 10),
-              height: 150,
-              child: Expanded(
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return HalamanPariwisata();
-                        }));
-                      },
-                      child: Container(
-                        margin: EdgeInsets.only(right: 10, left: 10),
-                        width: 165,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: AssetImage("img/read.png")),
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 5,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 4,
-                              offset: Offset(4, 8), // Shadow position
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        // Navigator.push(context,
-                        //     MaterialPageRoute(builder: (context) {
-                        //   return HalamanDetailLuarNegeri();
-                        // }));
-                      },
-                      child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        width: 165,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("img/pencil.png")),
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 5,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 4,
-                              offset: Offset(4, 8), // Shadow position
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-        ),);
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 0,
+        color: Colors.brown,
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Colors.brown,
+        height: 50,
+        items: <Widget>[
+          Icon(
+            Icons.menu_book_outlined,
+            size: 20,
+            color: Colors.white,
+          ),
+          Icon(Icons.add, size: 20, color: Colors.white),
+          Icon(Icons.person, size: 20, color: Colors.white),
+        ],
+        animationDuration: Duration(milliseconds: 200),
+        animationCurve: Curves.bounceInOut,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+      body: _widgetOptions[_selectedIndex],
+      backgroundColor: Colors.blueGrey[50],
+    );
   }
 }
